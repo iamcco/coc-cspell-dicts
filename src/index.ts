155203d6dbf932defa2a57f769e54b08060390b6
afreakk <laderud@hotmail.com>
2022-04-02 11:58:46 +0200

    Support 'newStyle' @cspell dicts + add norwegian
    + Bumped coc.nvim to 0.0.80, I couldn't make it build with older
      coc.nvim, because of some typescript errors in coc.nvim library.
    
    + Bumped @types/node to the same version as coc.nvim 0.0.80 uses
    
    + Added typescript as dev dependency, so you dont need it installed
      globally
    
    + If the dict is 'newStyle' use require.resolve() instead of
      require().getConfigLocation()
    
    + coc.nvim 0.0.80 doesnt export extensions.getExtension function to index.d.ts, (even
      though it exists in the sourcecode) so use extensions.all.find()
      instead.
    
    + Added norwegian (nb) dictionary, I set the local to the same as what
      is defined here https://github.com/streetsidesoftware/cspell-dicts/blob/main/dictionaries/nb_NO/cspell-ext.json#L28
    
    + Removed the catalan word inside the registerCommand's, cause im pretty
      sure that was not supposed to be there

--- src/index.ts
+++ src/index.ts
@@ -5,83 +5,89 @@ const dicts: Record<
   {
     local?: string;
     package: string | string[];
+    newStyle?: boolean;
   }
 > = {
   catalan: {
     local: "ca",
-    package: "cspell-dict-ca"
+    package: "cspell-dict-ca",
   },
   czech: {
     local: "cs",
-    package: "cspell-dict-cs-cz"
+    package: "cspell-dict-cs-cz",
+  },
+  norwegian: {
+    newStyle: true,
+    local: "nb, nb-no",
+    package: "@cspell/dict-nb-no",
   },
   danish: {
     local: "dk",
-    package: "cspell-dict-da-dk"
+    package: "cspell-dict-da-dk",
   },
   dutch: {
     local: "nl",
-    package: "cspell-dict-nl-nl"
+    package: "cspell-dict-nl-nl",
   },
   french: {
     local: "fr",
-    package: "cspell-dict-fr-fr"
+    package: "cspell-dict-fr-fr",
   },
   "french-reforme": {
     local: "fr-90",
-    package: "cspell-dict-fr-reforme"
+    package: "cspell-dict-fr-reforme",
   },
   german: {
     local: "de",
-    package: "cspell-dict-de-de"
+    package: "cspell-dict-de-de",
   },
   greek: {
     local: "el",
-    package: "cspell-dict-el"
+    package: "cspell-dict-el",
   },
   italian: {
     local: "it",
-    package: "cspell-dict-it-it"
+    package: "cspell-dict-it-it",
   },
   "medical-terms": {
-    package: "cspell-dict-medicalterms"
+    package: "cspell-dict-medicalterms",
   },
   persian: {
     local: "fa",
-    package: "cspell-dict-fa-ir"
+    package: "cspell-dict-fa-ir",
   },
   polish: {
     local: "pl",
-    package: "cspell-dict-pl_pl"
+    package: "cspell-dict-pl_pl",
   },
   portuguese: {
     local: "pt,pt_PT",
-    package: "cspell-dict-pt-pt"
+    package: "cspell-dict-pt-pt",
   },
   "portuguese-brazilian": {
     local: "pt,pt_BR",
-    package: "cspell-dict-pt-br"
+    package: "cspell-dict-pt-br",
   },
   russian: {
     local: "ru",
-    package: ["cspell-dict-ru_ru", "cspell-dict-russian"]
+    package: ["cspell-dict-ru_ru", "cspell-dict-russian"],
   },
   spanish: {
     local: "es",
-    package: "cspell-dict-es-es"
+    package: "cspell-dict-es-es",
   },
   swedish: {
     local: "sv",
-    package: "cspell-dict-sv"
+    package: "cspell-dict-sv",
   },
   turkish: {
     local: "tr",
-    package: "cspell-dict-tr-tr"
+    package: "cspell-dict-tr-tr",
   },
   ukrainian: {
     local: "uk",
-    package: "cspell-dict-uk-ua"
-  }
+    package: "cspell-dict-uk-ua",
+  },
 };
 
 // this method is called when your extension is activated
@@ -93,42 +99,56 @@ export function activate(context: coc.ExtensionContext) {
     .getConfiguration("cSpellExt")
     .get<string[]>("enableDictionaries", []);
 
-  const cocSpellChecker = coc.extensions.getExtension(cocSpellCheckerExtension)
+  // const cocSpellChecker = coc.extensions.getExtension(cocSpellCheckerExtension);
+  // hack: getExtension() is not defined in coc.nvim 0.0.80 index.d.ts for some reason, so use all.find() instead
+  const cocSpellChecker = coc.extensions.all.find(
+    (x) => x.id === cocSpellCheckerExtension
+  );
 
   if (cocSpellChecker) {
-    cocSpellChecker.extension.activate().then(ext => {
+    cocSpellChecker.activate().then((ext) => {
       if (enableDictionaries && enableDictionaries.length) {
-        enableDictionaries.forEach(lang => {
-          const dict = dicts[lang]
+        enableDictionaries.forEach((lang) => {
+          const dict = dicts[lang];
           if (dict) {
             if (dict.package) {
-              const packages = ([] as string[]).concat(dict.package)
-              packages.forEach(name => {
-                const p = require(name)
+              const packages = ([] as string[]).concat(dict.package);
+              packages.forEach((name) => {
+                const p = dict.newStyle ? require.resolve(name) : require(name);
                 if (p) {
-                  const path = p.getConfigLocation();
+                  const path = dict.newStyle ? p : p.getConfigLocation();
                   // We need to register the dictionary configuration with the Code Spell Checker Extension
                   ext && ext.registerConfig && ext.registerConfig(path);
                 }
-              })
+              });
             }
             if (dict.local) {
               // Push the disposable to the context's subscriptions so that the
               // client can be deactivated on extension deactivation
               context.subscriptions.push(
-                coc.commands.registerCommand(`cSpellExt_${lang}.enableCatalan`, () =>
-                  ext && ext.enableLocal && ext.enableLocal(true, dict.local)
-                ),
-                coc.commands.registerCommand(`cSpellExt_${lang}.disableCatalan`, () =>
-                  ext && ext.disableLocal && ext.disableLocal(true, dict.local)
+                coc.commands.registerCommand(
+                  `cSpellExt_${lang}.enable`,
+                  () =>
+                    ext && ext.enableLocal && ext.enableLocal(true, dict.local)
                 ),
                 coc.commands.registerCommand(
-                  `cSpellExt_${lang}.enableCatalanWorkspace`,
-                  () => ext && ext.enableLocal && ext.enableLocal(false, dict.local)
+                  `cSpellExt_${lang}.disable`,
+                  () =>
+                    ext &&
+                    ext.disableLocal &&
+                    ext.disableLocal(true, dict.local)
                 ),
                 coc.commands.registerCommand(
-                  `cSpellExt_${lang}.disableCatalanWorkspace`,
-                  () => ext && ext.disableLocal && ext.disableLocal(false, dict.local)
+                  `cSpellExt_${lang}.enableWorkspace`,
+                  () =>
+                    ext && ext.enableLocal && ext.enableLocal(false, dict.local)
+                ),
+                coc.commands.registerCommand(
+                  `cSpellExt_${lang}.disableWorkspace`,
+                  () =>
+                    ext &&
+                    ext.disableLocal &&
+                    ext.disableLocal(false, dict.local)
                 )
               );
             }
@@ -137,7 +157,9 @@ export function activate(context: coc.ExtensionContext) {
       }
     });
   } else {
-    coc.workspace.showMessage(`[coc-cspell-dicts]: require ${cocSpellCheckerExtension} extensions installed`)
+    coc.window.showMessage(
+      `[coc-cspell-dicts]: require ${cocSpellCheckerExtension} extensions installed`
+    );
   }
 }
 
